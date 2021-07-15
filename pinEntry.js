740367ae8f91be91d71a6ae9de147860aaacddd7
Matt Jarvis <matt.jarvis@gmail.com>
2021-07-15 14:36:16 +0100

    Add pin entry page.

---
+++ pinEntry.js
@@ -0,0 +1,40 @@
+window.attempts = 2
+
+function checkPin() {
+    const pinValue = document.querySelector("#pinEntry").value
+    if (pinValue !== "1234") {
+        showPinError()
+    }
+}
+
+function showPinError() {
+    document.querySelector("#pinEntry").classList.add("nhsuk-input--error")
+    document.querySelector("#pinEntryForm").classList.add("nhsuk-form-group--error")
+    if (attempts < 1) {
+        document.querySelector("#lockedOut").style.display = "block"
+    } else {
+        evaluateCooldown()
+        document.querySelector("#incorrectPin").style.display = "block"
+    }
+}
+
+function evaluateCooldown(delaySeconds = 5) {
+    if (delaySeconds < 1) {
+        //Cooldown over
+        window.attempts--
+        document.querySelector("#incorrectPin").style.display = "none"
+        document.querySelector("#pinEntry").disabled = false
+        document.querySelector("#submit").disabled = false
+    } else {
+        //Cooldown still going
+        document.querySelector("#cooldown").innerText = `Wait ${delaySeconds}s to retry. You have ${window.attempts} attempts remaining.`
+        document.querySelector("#incorrectPin").style.display = "block"
+        document.querySelector("#pinEntry").disabled = true
+        document.querySelector("#submit").disabled = true
+        setTimeout(() => evaluateCooldown(delaySeconds - 1), 1000)
+    }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("#submit").onclick = checkPin
+})
